60055f679f4eac819d7405117574cc5b0466a411
jf.giammari <giammari.jf@gmail.com>
2020-12-02 14:22:04 +0100

    Up

--- main.py
+++ main.py
@@ -1,119 +1,150 @@
-PCODE = [1,2,3,5,6,98,7,4]
+MEM = []
+PCODE = []
 PC = 0
 def charge(file):
     """
-    Charge le fichier contenant les instructions PCODE
+    Charge le fichier contenant les instructions MEM
     """
     with open('INST.txt') as f:
-        INST = []
+  
         for line in f:
             line = line.rstrip()
             if " " in line:
                 line = line.split(" ")
-                INST.append(line)
+                PCODE.append(line)
             else:
-                INST.append([line])
+                PCODE.append([line])
             if 'str' in line:
                 break
-    return INST
+    interprete()
         
 
-def interprete(INST):
+def interprete():
     """ Lance les instructions a partir de leurs nom """
-    global PCODE
+    global MEM
     global PC
-    while PC < len(INST):
-        line = INST[PC]
+    global PCODE
+    while PC < len(PCODE):
+        inst = PCODE[PC]
         try:
-            if len(line) == 1:eval(line[0]+"()")
-            else:eval(line[0]+"("+line[1]+")")
+            if len(inst) == 1:eval(inst[0]+"()")
+            else:eval(inst[0]+"("+inst[1]+")")
+            print(MEM)
    
         except NameError:
-            print("ERREUR : "+str(line[0])+" n'existe pas (Ligne : "+str(PC)+")")
+            print("ERREUR : "+str(inst[0])+" n'existe pas (Ligne : "+str(PC)+")")
         PC+=1
-        print("PC : "+str(PC))
+       
         
 
 
 def ADD():
     """Additionne le sous-sommet de pile et le sommet, laisse
     le résultat au sommet."""
-    PCODE.append(PCODE[-1]+PCODE[-2])
+    MEM.append(MEM[-1]+MEM[-2])
 
 def SUB():
     """Soustrait le sous-sommet de pile et le sommet, laisse
     le résultat au sommet. """
-    PCODE.append(PCODE[-2]-PCODE[-1])
+    MEM.append(MEM[-2]-MEM[-1])
 
 def MUL():
     """Multiplie le sous-sommet de pile et le sommet, laisse
     le résultat au sommet."""
-    PCODE.append(PCODE[-1]*PCODE[-2])
+    MEM.append(MEM[-1]*MEM[-2])
 
 def DIV():
     """Divise le sous-sommet de pile et le sommet, laisse
     le résultat au sommet. """
-    PCODE.append(PCODE[-2]//PCODE[-1])
+    MEM.append(MEM[-2]//MEM[-1])
 
 def EQL():
     """Laisse 1 au sommet de pile si sous-sommet = sommet, 0 sinon"""
-    if PCODE[-1] == PCODE[-2]: PCODE.append(1)
-    else: PCODE.append(0)
+    if MEM[-1] == MEM[-2]: MEM.append(1)
+    else: MEM.append(0)
  
 
 def NEQ():
     """Laisse 1 au sommet de pile si sous-sommet != sommet, 0 sinon"""
-    if PCODE[-1] != PCODE[-2]: PCODE.append(1)
-    else: PCODE.append(0)
+    if MEM[-1] != MEM[-2]: MEM.append(1)
+    else: MEM.append(0)
 
+def GTR():
+    """Laisse 1 au sommet de pile si sous-sommet > sommet, 0 sinon"""
+    if MEM[-1] > MEM[-2]: MEM.append(1)
+    else: MEM.append(0)
+
+def LSS():
+    """Laisse 1 au sommet de pile si sous-sommet < sommet, 0 sinon"""
+    if MEM[-1] < MEM[-2]: MEM.append(1)
+    else: MEM.append(0)
+
+
+def GEQ():
+    """Laisse 1 au sommet de pile si sous-sommet >= sommet, 0 sinon"""
+    if MEM[-1] >= MEM[-2]: MEM.append(1)
+    else: MEM.append(0)
+
+def LEQ():
+    """Laisse 1 au sommet de pile si sous-sommet <= sommet, 0 sinon"""
+    if MEM[-1] <= MEM[-2]: MEM.append(1)
+    else: MEM.append(0)
 
 def PRN():
     """ Imprime le sommet et dépile """
-    print(PCODE[-1])
-    print("F")
+    print(MEM[-1])
 
-def INN(): #####################
+def INN(): 
     """Lit un entier, le stocke à l'adresse trouvée au sommet de
     pile, dépile """
-    PCODE.append(int(input("Saisi d'un entier :")))
+    MEM[MEM[-1]] = int(input("Saisi d'un entier :"))
+    MEM.pop(-1)
 
 def INT(p):
     """Incrémente de la constante p le pointeur de pile"""
     global PC
-    PC += p
-    print(p)
-    
+    for p in range(p):
+        MEM.append(0)
+        pass
+
 def LDI(p):
     """Empile la valeur p"""
-    PCODE.append(p)
+    MEM.append(p)
 
 
 def LDA(p):
     """Empile l'adresse p"""
-    PCODE.append(PCODE[p])
+    MEM.append(p)
 
 def LDV():
     """Remplace le sommet par la valeur trouvée à l'adresse
     indiquée par le sommet (déréférence) """
-    PCODE.append(PCODE[PCODE[-1]])
-    PCODE.pop(-2)
+    MEM.append(MEM[MEM[-1]])
+    MEM.pop(-2)
 
 
 def STO():
     """Stocke la valeur au sommet à l'adresse indiquée par le
     sous-sommet, dépile 2 fois """
-    print("F")
+    MEM[MEM[-2]] = MEM[-1]
+    MEM.pop(-1)
+    MEM.pop(-1)
+
 
 def BRN(p):
     """Branchement inconditionnel à l'instruction p"""
+    PC = p
 
 
 def BZE(p):
     """Branchement à l'instruction p si le sommet = 0, dépile"""
+    if MEM[-1] == 0:
+        PC = p
+    
 
 
 def HLT():
     """Halte"""
     return None
 
-interprete(charge("INST.txt"))
+charge("INST.txt")
